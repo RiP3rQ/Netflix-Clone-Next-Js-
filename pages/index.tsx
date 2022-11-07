e2e2a0c02bec13b454c518518d47d1524547ec23
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-07 19:07:31 +0100

    feat: creating banner component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[$010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -15,6 +16,7 @@ const Home: NextPage = () => {
       <Header />
       <main>
         {/* Banner Component */}
+        <Banner />
         <section>
           {/* Row */}
           {/* Row */}
