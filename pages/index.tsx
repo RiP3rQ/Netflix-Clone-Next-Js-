da181b4ce01ec878d46b6c3a9dbf657a582f34a9
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-07 18:11:53 +0100

    feat: header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {/* Header Component */}
+      <Header />
       <main>
         {/* Banner Component */}
         <section>
