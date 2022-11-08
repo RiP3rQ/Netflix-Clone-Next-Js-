5a7ef2281b1811e1f213568d71343ca6a1222594
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-08 21:05:34 +0100

    feat: row component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Row from "../components/Row";
 import { Movie } from "../typings";
 import requests from "../utils/requests";
 
@@ -28,7 +29,7 @@ const Home = ({
 }: Props) => {
   return (
     <div
-      className="relative h-screen overflow-hidden bg-gradient-to-b 
+      className="relative h-screen bg-gradient-to-b 
     from-gray-900/10 to-[#010511] lg:h-[140vh]"
     >
       <Head>
@@ -42,12 +43,17 @@ const Home = ({
         {/* Banner Component */}
         <Banner netflixOriginals={netflixOriginals} />
         <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+          {/* My List */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
+          {/* Rows */}
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* Modal Component*/}
