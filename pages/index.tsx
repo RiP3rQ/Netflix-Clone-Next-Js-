2112afa6b8e8b9883b4e113ddb0abb588a9ed050
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-08 19:41:53 +0100

    feat: tmdb api

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,10 @@ import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div
+      className="relative h-screen bg-gradient-to-b from-gray-900/10 
+    to-[#010511] lg:h-[140vh]"
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
