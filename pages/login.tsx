a4a0228f701fc5133b2140f8c74fd48a406f7d8e
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-09 20:51:32 +0100

    feat: import AuthProvider

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import useAuth from "../hooks/useAuth";
 
 interface Inputs {
   email: string;
@@ -10,6 +11,7 @@ interface Inputs {
 
 function Login() {
   const [login, setLogin] = useState(false);
+  const { signIn, signUp } = useAuth();
 
   const {
     register,
@@ -20,9 +22,9 @@ function Login() {
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     console.log(data);
     if (login) {
-      // await signIn(data.email, data.password);
+      await signIn(data.email, data.password);
     } else {
-      // await signUp(data.email, data.password);
+      await signUp(data.email, data.password);
     }
   };
 
