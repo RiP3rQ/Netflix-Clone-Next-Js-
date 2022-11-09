1a42032652e4bebfdac4d7909db1375668f0fb96
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-09 19:32:18 +0100

    feat: login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,28 @@
+import Head from "next/head";
+import Image from "next/image";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+        src="https://rb.gy/p2hphi"
+        layout="fill"
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        objectFit="cover"
+        alt=""
+      />
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+        width={150}
+        height={150}
+      />
+    </div>
+  );
 }
 
 export default Login;
