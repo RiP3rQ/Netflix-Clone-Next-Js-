d9691049c7c68165b45ef912bbddddba455fe730
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-09 19:51:57 +0100

    feat: login page - login form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
 
 function Login() {
+  const [login, setLogin] = useState(false);
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -21,6 +24,33 @@ function Login() {
         width={150}
         height={150}
       />
+
+      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+        <h1 className="text-4xl font-semibold">Sign In</h1>
+
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input type="email" placeholder="Email" className="input" />
+          </label>
+          <label htmlFor="">
+            <input type="password" placeholder="Password" className="input" />
+          </label>
+        </div>
+
+        <button className="w-full rounded bg-[#E50914] py-3 font-semibold">
+          Sign In
+        </button>
+
+        <div className="text-[gray]">
+          New to Netflix?{" "}
+          <button
+            className="cursor-pointer text-white hover:underline"
+            type="submit"
+          >
+            Sign up now
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
