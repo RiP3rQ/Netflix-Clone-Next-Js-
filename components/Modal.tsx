90cdd9b72f64713b21851a549eb2cb9cc1562756
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-10 20:34:47 +0100

    feat: reactPlayer package

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,10 +1,36 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import MuiModal from "@mui/material/Modal";
+import { setRevalidateHeaders } from "next/dist/server/send-payload";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
+import { Movie } from "../typings";
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState);
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (!movie) return;
+
+    async function fetchMovie() {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/${
+          movie?.media_type === "tv" ? "tv" : "movie"
+        }/${movie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&language=en-US&append_to_response=videos`
+      ).then((response) => response.json());
+
+      if(data?.)
+      setData(data);
+    }
+
+    fetchMovie();
+  }, [movie]);
+
+  console.log(data);
 
   const handleClose = () => {
     setShowModal(false);
@@ -14,7 +40,10 @@ function Modal() {
     <MuiModal open={showModal} onClose={handleClose}>
       <>
         <button>
-          <XMarkIcon className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]" />
+          <XMarkIcon
+            className="modalButton absolute right-5 top-5 !z-40 
+          h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+          />
         </button>
       </>
     </MuiModal>
