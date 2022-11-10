27ef0fda7492883dd1ec4374ab8074b14f0fb663
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-10 22:05:59 +0100

    feat: fetch movies from api for modal + modal styling + design

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,15 +1,24 @@
-import { XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  HandThumbUpIcon,
+  PlusIcon,
+  SpeakerWaveIcon,
+  SpeakerXMarkIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 import MuiModal from "@mui/material/Modal";
-import { setRevalidateHeaders } from "next/dist/server/send-payload";
 import { useEffect, useState } from "react";
+import { FaPlay } from "react-icons/fa";
+import ReactPlayer from "react-player/lazy";
 import { useRecoilState } from "recoil";
-import { modalState } from "../atoms/modalAtom";
-import { Movie } from "../typings";
+import { modalState, movieState } from "../atoms/modalAtom";
+import { Element, Genre, Movie } from "../typings";
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState);
-  const [movie, setMovie] = useState<Movie | null>(null);
-  const [data, setData] = useState();
+  const [movie, setMovie] = useRecoilState(movieState);
+  const [trailer, setTrailer] = useState("");
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [muted, setMuted] = useState(true);
 
   useEffect(() => {
     if (!movie) return;
@@ -23,28 +32,109 @@ function Modal() {
         }&language=en-US&append_to_response=videos`
       ).then((response) => response.json());
 
-      if(data?.)
-      setData(data);
+      if (data?.videos) {
+        const index = data.videos.results.findIndex(
+          (element: Element) => element.type === "Trailer"
+        );
+        setTrailer(data.videos?.results[index]?.key);
+      }
+
+      if (data?.genres) {
+        setGenres(data.genres);
+      }
     }
 
     fetchMovie();
   }, [movie]);
-
-  console.log(data);
 
   const handleClose = () => {
     setShowModal(false);
   };
 
   return (
-    <MuiModal open={showModal} onClose={handleClose}>
+    <MuiModal
+      open={showModal}
+      onClose={handleClose}
+      className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+    >
       <>
-        <button>
-          <XMarkIcon
-            className="modalButton absolute right-5 top-5 !z-40 
+        <button
+          className="modalButton absolute right-5 top-5 !z-40 
           h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
-          />
+          onClick={handleClose}
+        >
+          <XMarkIcon className="h-6 w-6" />
         </button>
+        <div className="relative pt-[56.25%]">
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailer}`}
+            width="100%"
+            height="100%"
+            style={{ position: "absolute", top: "0", left: "0" }}
+            playing
+            muted={muted}
+          />
+          <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+            <div className="flex space-x-2">
+              <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
+                <FaPlay className="h-7 w-7 text-black" />
+                Play
+              </button>
+
+              <button className="modalButton">
+                <PlusIcon className="h-7 w-7" />
+              </button>
+
+              <button className="modalButton">
+                <HandThumbUpIcon className="h-7 w-7" />
+              </button>
+            </div>
+
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted ? (
+                <SpeakerXMarkIcon className="h-6 w-6" />
+              ) : (
+                <SpeakerWaveIcon className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+                HD
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres:</span>{" "}
+                  {genres.map((genre) => genre.name).join(", ")}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Original language:</span>{" "}
+                  {movie?.original_language}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Total votes:</span>{" "}
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     </MuiModal>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -37,8 +37,9 @@ const Home = ({
 
   return (
     <div
-      className="relative h-screen 
-    bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]"
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && "!h-screen overflow-hidden"
+      }`}
     >
       <Head>
         <title>Home - Netflix</title>
