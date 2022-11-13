5c5cb9294981ac1d92854f5c29041cfc1bde8f63
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-13 20:06:00 +0100

    feat: plans table + styling

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -2,7 +2,9 @@ import { CheckIcon } from "@heroicons/react/24/outline";
 import { Product } from "@stripe/firestore-stripe-payments";
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
 import useAuth from "../hooks/useAuth";
+import Table from "./Table";
 
 interface Props {
   products: Product[];
@@ -10,6 +12,7 @@ interface Props {
 
 function Plans({ products }: Props) {
   const { logout } = useAuth();
+  const [selectedPlan, setSelectedPlan] = useState<Product | null>(products[2]);
 
   return (
     <div>
@@ -58,12 +61,22 @@ function Plans({ products }: Props) {
         <div className="mt-4 flex flex-col space-y-4">
           <div className="flex w-full items-center justify-end self-end md:w-3/5">
             {/* PLANS */}
-            <div className="planBox">Standard</div>
-            <div className="planBox">Premium</div>
-            <div className="planBox">Ultra</div>
+            {products.map((product) => (
+              <div
+                key={product.id}
+                className={`planBox ${
+                  selectedPlan?.id === product.id ? "opacity-100" : "opacity-60"
+                }`}
+                onClick={() => setSelectedPlan(product)}
+              >
+                {product.name}
+              </div>
+            ))}
           </div>
 
-          {/* <Table/> */}
+          {/* Table */}
+          <Table products={products} selectedPlan={selectedPlan} />
+
           <button>Subscribe</button>
         </div>
       </main>
