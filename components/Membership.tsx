f3bc36d2d92314fa608b7daf653afd36ec0c63b0
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-14 20:54:48 +0100

    feat: membership component + styling

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -12,7 +12,7 @@ function Membership() {
   const manageSubscription = () => {
     if (subscription) {
       setBillingLoading(true);
-      //   goToBillingPortal();
+      goToBillingPortal();
     }
   };
 
@@ -23,14 +23,44 @@ function Membership() {
         <button
           disabled={isBillingLoading || !subscription}
           className="h-10 w-3/5 whitespace-nowrap bg-gray-300 py-2 text-sm font-medium text-black shadow-md hover:bg-gray-200 md:w-4/5"
-          //   onClick={manageSubscription}
+          onClick={manageSubscription}
         >
           {isBillingLoading ? (
-            <Loader color="dark:fill-[#e50914]" />
+            <Loader color="fill-[#e50914]" />
           ) : (
-            "Cancel Membership"
+            "Manage Membership"
           )}
         </button>
+      </div>
+
+      <div className="col-span-3">
+        <div className="flex flex-col justify-between border-b border-white/10 py-4 md:flex-row">
+          <div>
+            <p className="font-medium">{user?.email}</p>
+            <p className="text-[gray]">Password: ********</p>
+          </div>
+          <div className="md:text-right">
+            <p className="membershipLink">Change email</p>
+            <p className="membershipLink">Change password</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-between pt-4 pb-4 md:flex-row md:pb-0">
+          <div>
+            <p>
+              {subscription?.cancel_at_period_end
+                ? "Your membership will end on "
+                : "Your next billing date is "}
+              {subscription?.current_period_end}
+            </p>
+          </div>
+          <div className="md:text-right">
+            <p className="membershipLink">Manage payment info</p>
+            <p className="membershipLink">Add backup payment method</p>
+            <p className="membershipLink">Billing Details</p>
+            <p className="membershipLink">Change billing day</p>
+          </div>
+        </div>
       </div>
     </div>
   );
